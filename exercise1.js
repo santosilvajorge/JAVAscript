b0c13b9b19a3f8645e936e38036879fbb4db366c
santosilvajorge <santosilvajorge@gmail.com>
2022-04-01 19:01:58 +0100

    update 1.1

---
+++ exercise1.js
@@ -0,0 +1,73 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+*/
+
+/* EXERCISE 1
+ Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
+
+/*`There are 5 main dataTypes in javaScript:
+ - strings - are a text that comes inbetween "" or ''
+ - numbers - is like the name said a number
+ - booleans - is a True/False information about a particular case
+ - undefined - is like the name said something that is not defined
+ - null - is a null value ´
+*/
+
+/* EXERCISE 2
+ Try to describe what an object is, in your own words.
+*/
+
+/* "Objects you can think it like a box where you can add all diferent types of data in order to create a single structure." */
+
+/* EXERCISE 3
+ Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+let firstVar = 12
+let secondVar =20
+
+console.log(firstVar + secondVar)
+
+/* EXERCISE 4
+ Create a variable named x and assign to it the number 12.
+*/
+
+/* let xVar = 12 */
+
+/* EXERCISE 5
+ Create a variable called name and assign to it your name as a string.
+*/
+
+/* let nameVar = "Jorge" */
+
+/* EXERCISE 6
+ Execute a subtraction between the number 4 and the variable x you declared before (which is storing the value 12).
+*/
+
+/* console.log(xVar - 4) */
+
+/* EXERCISE 7
+Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
+ Verify that name1 is different from name2 (HINT: check their equality to be false).
+ Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
+ YOU DON'T NEED AN IF/ELSE BLOCK. It is enough to use console.log()
+*/
+
+/* 
+let name1Var = "john"
+let name2Var = "John"
+
+let lowerCase = "john"
+console.log() 
+*/
+
+
+/* WHEN YOU ARE FINISHED
+ Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
+*/
