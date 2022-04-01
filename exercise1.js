04f6360ac03a98721410819aa700eba037cd5a15
santosilvajorge <santosilvajorge@gmail.com>
2022-04-01 19:10:32 +0100

    final update 1.3\
    Just some last updates to read better the exercise inbetween the "Exercises" numbers

--- exercise1.js
+++ exercise1.js
@@ -7,7 +7,9 @@ ASSIGNMENT RULES
 - You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
 */
 
-/* EXERCISE 1
+
+ 'EXERCISE 1'
+ /*
  Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
 */
 
@@ -19,13 +21,15 @@ ASSIGNMENT RULES
  - null - is a null value ´
 */
 
-/* EXERCISE 2
+ 'EXERCISE 2'
+ /*
  Try to describe what an object is, in your own words.
 */
 
 /* "Objects you can think it like a box where you can add all diferent types of data in order to create a single structure." */
 
-/* EXERCISE 3
+ 'EXERCISE 3'
+ /*
  Write the code to execute an addition (a sum) of the numbers 12 and 20.
 */
 
@@ -35,25 +39,29 @@ let secondVar =20
 console.log(firstVar + secondVar)
 */
 
-/* EXERCISE 4
+ 'EXERCISE 4'
+ /*
  Create a variable named x and assign to it the number 12.
 */
 
 /* let xVar = 12 */
 
-/* EXERCISE 5
+ 'EXERCISE 5'
+ /*
  Create a variable called name and assign to it your name as a string.
 */
 
 /* let nameVar = "Jorge" */
 
-/* EXERCISE 6
+ 'EXERCISE 6'
+ /*
  Execute a subtraction between the number 4 and the variable x you declared before (which is storing the value 12).
 */
 
 /* console.log(xVar - 4) */
 
-/* EXERCISE 7
+ 'EXERCISE 7'
+ /*
 Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
  Verify that name1 is different from name2 (HINT: check their equality to be false).
  Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
@@ -65,8 +73,4 @@ let name1Var = "john"
 let name2Var = "John"
 
 console.log(name1Var == name2Var) 
-*/
-
-/* WHEN YOU ARE FINISHED
- Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
 */
